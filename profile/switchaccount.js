bf034b9c09a88704c317efd44b39bd3b6d29c4cf
Mounikagaiansolution <mounika.koseti@gaiansolution.com>
2024-05-17 17:28:44 +0530

    Adding avatar and password screens

---
+++ profile/switchaccount.js
@@ -0,0 +1,8 @@
+document.getElementById("manageAccount").addEventListener("click", function() {
+
+    window.location.href = "editdetails.html";
+});
+document.getElementById("addUser").addEventListener("click", function() {
+
+    window.location.href = "manageaccount.html";
+});
