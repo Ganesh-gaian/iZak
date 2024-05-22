bba9785a2af1ca44be0c51d4bbf10e592a1e7b91
Shubham Lahase <shubham.sheshrao@gaiansolutions.com>
2024-05-22 10:38:27 +0530

    some css chnages and id for navigation events added for the user journey

--- spaces/space.js
+++ spaces/space.js
@@ -1,10 +1,32 @@
-function selectButton(button) {
-    // Remove the 'selected' class from all buttons
-    var buttons = document.getElementsByClassName('button');
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].classList.remove('selected');
-    }
-    // Add the 'selected' class to the clicked button
-    button.classList.add('selected');
-  }
-  
+const btn1 = document.getElementById("b1");
+const btn2 = document.getElementById("b2");
+const btn3 = document.getElementById("b3");
+const btn4 = document.getElementById("b4");
+
+btn1.addEventListener("click", function () {
+  btn1.classList.add("selected");
+  btn2.classList.remove("selected");
+  btn3.classList.remove("selected");
+  btn4.classList.remove("selected");
+});
+
+btn2.addEventListener("click", function () {
+  btn1.classList.remove("selected");
+  btn2.classList.add("selected");
+  btn3.classList.remove("selected");
+  btn4.classList.remove("selected");
+});
+
+btn3.addEventListener("click", function () {
+  btn1.classList.remove("selected");
+  btn2.classList.remove("selected");
+  btn3.classList.add("selected");
+  btn4.classList.remove("selected");
+});
+
+btn3.addEventListener("click", function () {
+  btn1.classList.remove("selected");
+  btn2.classList.remove("selected");
+  btn3.classList.remove("selected");
+  btn4.classList.add("selected");
+});
