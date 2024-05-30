fb08936899ac4822d29668015c7da47ec900552f
SaddamGaian <mohammad.saddam@gaiansolutions.com>
2024-05-30 18:21:45 +0530

    added vaccum and monoxide cards

--- devicesCards/miniCards/manual/index.js
+++ devicesCards/miniCards/manual/index.js
@@ -1,131 +1,189 @@
 const devices = [
-    {
-        id: "updateCameraDevice",
-        title: "Camera",
-        location: "TP Link • Outdoor Area",
-        isNew: true,
-        statusImg: "./camera-img.svg",
-        background: "linear-gradient(128deg, #00478e -5.15%, #0b82f970, #c975fcb8 105.74%)",
-        boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
-        lastUsed: {
-            action: "Manual Trigger",
-            time: "12:00 AM",
-        },
-        nextScheduled: false
+  {
+    id: "updateCameraDevice",
+    title: "Camera",
+    location: "TP Link • Outdoor Area",
+    isNew: true,
+    statusImg: "./camera-img.svg",
+    background:
+      "linear-gradient(128deg, #00478e -5.15%, #0b82f970, #c975fcb8 105.74%)",
+    boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+    lastUsed: {
+      action: "Manual Trigger",
+      time: "12:00 AM",
     },
-    {
-        id: "updateCarbonMono",
-        title: "Carbon Monoxide Detector",
-        location: "SONOFF • Master Bedroom",
-        isNew: true,
-        statusImg: "./ppm.svg",
-        background: "linear-gradient(128deg, #00478e -5.15%, #0b82f970, #c975fcb8 105.74%)",
-        boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
-        lastUsed: {
-            action: "Manual Trigger",
-            time: "12:00 AM",
-        },
-        nextScheduled: false
-    }
-
+    nextScheduled: false,
+  },
+  {
+    id: "updateCarbonMono",
+    title: "Carbon Monoxide Detector",
+    location: "SONOFF • Master Bedroom",
+    isNew: false,
+    statusImg: "./ppm.svg",
+    background:
+      "linear-gradient(128deg, #00478e -5.15%, #0b82f970, #c975fcb8 105.74%)",
+    boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+    lastUsed: {
+      action: "Manual Trigger",
+      time: "12:00 AM",
+    },
+    nextScheduled: false,
+  },
+  {
+    id: "updateCarbonMonoBg",
+    title: "Carbon Monoxide Detector",
+    location: "SONOFF • Master Bedroom",
+    isNew: false,
+    statusImg: "./ppm.svg",
+    background: "#6a6969",
+    boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+    lastUsed: {
+      action: "Manual Trigger",
+      time: "12:00 AM",
+    },
+    nextScheduled: false,
+  },
+  {
+    id: "updateVaccum",
+    title: "Vaccum",
+    location: "iRobot • Living Room",
+    isNew: false,
+    statusImg: "./wirelessVaccumCleaner.svg",
+    background:
+      "linear-gradient(128deg, #00478e -5.15%, #0b82f970, #c975fcb8 105.74%)",
+    boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+    lastUsed: {
+      action: "Manual Trigger",
+      time: "12:00 AM",
+    },
+    nextScheduled: true,
+  },
+  {
+    id: "updateVaccumBg",
+    title: "Vaccum",
+    location: "iRobot • Living Room",
+    isNew: false,
+    statusImg: "./wirelessVaccumCleaner.svg",
+    background: "#6a6969",
+    boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+    lastUsed: {
+      action: "Manual Trigger",
+      time: "12:00 AM",
+    },
+    nextScheduled: true,
+  },
 ];
 
 function createDeviceCard(device) {
-    const deviceCont = document.createElement("div");
-    deviceCont.className = "deviceCont";
-    deviceCont.style.background = device.background;
-    deviceCont.style.boxShadow = device.boxShadow;
+  const deviceCont = document.createElement("div");
+  deviceCont.className = "deviceCont";
+  deviceCont.style.background = device.background;
+  deviceCont.style.boxShadow = device.boxShadow;
 
-    const span85 = document.createElement("span");
-    span85.id = "span85";
-    span85.textContent = device.title;
+  const span85 = document.createElement("span");
+  span85.id = "span85";
+  span85.textContent = device.title;
 
-    const span95 = document.createElement("span");
-    span95.id = "span95";
-    span95.textContent = device.location;
+  const span95 = document.createElement("span");
+  span95.id = "span95";
+  span95.textContent = device.location;
 
-    const newDiv = document.createElement("div");
-    newDiv.className = "newud";
-    newDiv.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+  const newDiv = document.createElement("div");
+  newDiv.className = "newud";
+  newDiv.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
                           <circle cx="6" cy="6" r="6" fill="#FF0606"/>
                         </svg><span>Live</span>`;
-    if (!device.isNew) newDiv.style.display = "none";
+  if (!device.isNew) newDiv.style.display = "none";
 
-    const statusCont = document.createElement("div");
-    statusCont.className = "statusCont";
+  const statusCont = document.createElement("div");
+  statusCont.className = `statusCont ${device.id}`;
 
-    const statusImg = document.createElement("img");
-    statusImg.src = device.statusImg;
-    statusImg.alt = "";
+  const statusImg = document.createElement("img");
+  statusImg.src = device.statusImg;
+  statusImg.alt = "";
 
-    const dbottomCont = document.createElement("div");
-    dbottomCont.className = "dbottomCont";
+  if (device.id === "updateCarbonMono" || device.id === "updateCarbonMonoBg") {
+    const statusText = document.createElement("span");
+    statusText.className = "statusText";
+    statusText.textContent = "574 ppm";
+    statusCont.appendChild(statusText);
+  }
 
-    const dbLetfCont = document.createElement("div");
-    dbLetfCont.className = "dbLetfCont";
+  const dbottomCont = document.createElement("div");
+  dbottomCont.className = "dbottomCont";
 
-    const span129 = document.createElement("span");
-    span129.id = "span129";
-    span129.textContent = "Last used";
+  const dbLetfCont = document.createElement("div");
+  dbLetfCont.className = "dbLetfCont";
 
-    const span139 = document.createElement("span");
-    span139.id = "span139";
-    span139.textContent = device.lastUsed.action;
+  const span129 = document.createElement("span");
+  span129.id = "span129";
+  span129.textContent = "Last used";
 
-    const span149 = document.createElement("span");
-    span149.id = "span149";
-    span149.textContent = device.lastUsed.time;
+  const span139 = document.createElement("span");
+  span139.id = "span139";
+  span139.textContent = device.lastUsed.action;
 
-    dbLetfCont.appendChild(span129);
-    dbLetfCont.appendChild(span139);
-    dbLetfCont.appendChild(span149);
+  const span149 = document.createElement("span");
+  span149.id = "span149";
+  span149.textContent = device.lastUsed.time;
 
-    dbottomCont.appendChild(dbLetfCont);
+  dbLetfCont.appendChild(span129);
+  dbLetfCont.appendChild(span139);
+  dbLetfCont.appendChild(span149);
 
-    if (device.nextScheduled) {
-        const dbRightCont = document.createElement("div");
-        dbRightCont.className = "dbRightCont";
+  dbottomCont.appendChild(dbLetfCont);
 
-        const span120 = document.createElement("span");
-        span120.id = "span120";
-        span120.textContent = "Next Scheduled for";
+  if (device.nextScheduled) {
+    const dbRightCont = document.createElement("div");
+    dbRightCont.className = "dbRightCont";
 
-        const span130 = document.createElement("span");
-        span130.id = "span130";
-        span130.textContent = device.nextScheduled.action;
+    const imageDiv = document.createElement("div");
+    imageDiv.className = "ImgDiv";
+    const image80px = document.createElement("img");
+    image80px.src = "Quiet Zone.svg";
+    image80px.className = "imgAboveTextManual";
+    imageDiv.appendChild(image80px);
+    dbRightCont.appendChild(imageDiv);
 
-        const span140 = document.createElement("span");
-        span140.id = "span140";
-        span140.textContent = device.nextScheduled.time;
+    const span120 = document.createElement("span");
+    span120.id = "span120";
+    span120.textContent = "Quiet Mode";
 
-        dbRightCont.appendChild(span120);
-        dbRightCont.appendChild(span130);
-        dbRightCont.appendChild(span140);
+    const span130 = document.createElement("span");
+    span130.id = "span130";
+    span130.textContent = device.nextScheduled.action;
 
-        dbottomCont.appendChild(dbRightCont);
-    }
+    const span140 = document.createElement("span");
+    span140.id = "span140";
+    span140.textContent = device.nextScheduled.time;
 
-    statusCont.appendChild(statusImg);
+    dbRightCont.appendChild(span120);
+    dbRightCont.appendChild(span130);
+    dbRightCont.appendChild(span140);
 
-    deviceCont.appendChild(span85);
-    deviceCont.appendChild(span95);
-    deviceCont.appendChild(newDiv);
-    deviceCont.appendChild(statusCont);
-    deviceCont.appendChild(dbottomCont);
+    dbottomCont.appendChild(dbRightCont);
+  }
 
-    return deviceCont;
+  statusCont.appendChild(statusImg);
+
+  deviceCont.appendChild(span85);
+  deviceCont.appendChild(span95);
+  deviceCont.appendChild(newDiv);
+  deviceCont.appendChild(statusCont);
+  deviceCont.appendChild(dbottomCont);
+
+  return deviceCont;
 }
 
 function displayDevice(deviceId) {
-    const contentContainer = document.getElementById("contentContainer");
-    contentContainer.innerHTML = "";
+  const contentContainer = document.getElementById("contentContainer");
+  contentContainer.innerHTML = "";
 
-    const device = devices.find((d) => d.id === deviceId);
-    if (device) {
-        const deviceCard = createDeviceCard(device);
-        contentContainer.appendChild(deviceCard);
-    }
+  const device = devices.find((d) => d.id === deviceId);
+  if (device) {
+    const deviceCard = createDeviceCard(device);
+    contentContainer.appendChild(deviceCard);
+  }
 }
 
-// Display the device with ID 'updateCameraDevice'
-displayDevice("updateCameraDevice");
+displayDevice("updateVaccumBg");
---
+++ devicesCards/vaccum/index.js
@@ -0,0 +1,78 @@
+const favBtn = document.getElementById("fav");
+const infoBtn = document.getElementById("info");
+const recycleBtn = document.getElementById("recycle");
+
+favBtn.addEventListener("click", () => {
+  favBtn.classList.toggle("colored");
+});
+infoBtn.addEventListener("click", () => {
+  infoBtn.classList.toggle("colored");
+});
+recycleBtn.addEventListener("click", () => {
+  recycleBtn.classList.toggle("colored");
+});
+
+//for Quiet spireal etc buttons:
+const quietModeBtn = document.getElementById("quietModeBtn");
+const spiralModeBtn = document.getElementById("spiralModeBtn");
+const chargingModeBtn = document.getElementById("chargingModeBtn");
+const sideWallModeBtn = document.getElementById("sideWallModeBtn");
+
+function removeColoredClass() {
+  quietModeBtn.classList.remove("colored");
+  spiralModeBtn.classList.remove("colored");
+  chargingModeBtn.classList.remove("colored");
+  sideWallModeBtn.classList.remove("colored");
+}
+
+quietModeBtn.addEventListener("click", () => {
+  removeColoredClass();
+  quietModeBtn.classList.add("colored");
+});
+
+spiralModeBtn.addEventListener("click", () => {
+  removeColoredClass();
+  spiralModeBtn.classList.add("colored");
+});
+
+chargingModeBtn.addEventListener("click", () => {
+  removeColoredClass();
+  chargingModeBtn.classList.add("colored");
+});
+
+sideWallModeBtn.addEventListener("click", () => {
+  removeColoredClass();
+  sideWallModeBtn.classList.add("colored");
+});
+
+
+// js for input range slider
+const intensityRange = document.getElementById("intensityRange");
+const intensityCount = document.getElementById("intensityCount");
+
+intensityRange.addEventListener("input", () => {
+  intensityCount.textContent = `${intensityRange.value}%`;
+});
+//timer
+document.addEventListener('DOMContentLoaded', () => {
+  const timerInputs = document.querySelectorAll('.timer-box');
+
+  timerInputs.forEach(input => {
+    input.addEventListener('input', () => {
+      if (input.value.length > 2) {
+        input.value = input.value.slice(0, 2);
+      }
+    });
+    
+    input.addEventListener('keypress', (e) => {
+      if (e.key < '0' || e.key > '9') {
+        e.preventDefault();
+      }
+    });
+
+    input.addEventListener('focus', (e) => {
+      e.target.select();
+    });
+  });
+});
+
