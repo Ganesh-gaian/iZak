f5f0b34165f248667dc0ed141f740c8696fbc3ee
Mounikagaiansolution <mounika.koseti@gaiansolution.com>
2024-06-05 17:59:54 +0530

    Updated js functionality in edit playbook

--- playbook/editplaybooktriggers.js
+++ playbook/editplaybooktriggers.js
@@ -6,14 +6,25 @@ document.querySelector(".back-arrow").addEventListener("click", function () {
 function selectOption(option) {
   // Remove 'selected' class from all options
   var options = document.querySelectorAll(".option");
+  var allimages =document.querySelectorAll(".arrow")
+  allimages.forEach(function (opt) {
+    opt.style.visibility="hidden"
+
+  });
+
   options.forEach(function (opt) {
     opt.classList.remove("selected");
   });
+
 
   // Add 'selected' class to the clicked option
   var selectedOption = document.querySelector("." + option);
   if (selectedOption) {
     selectedOption.classList.add("selected");
+  }
+  var imgHtml =document.getElementById(option)
+  if(imgHtml){
+    imgHtml.style.visibility="visible"
   }
 
   var detailsDiv = document.getElementById("details-" + option);
@@ -33,13 +44,32 @@ function redirect(buttonId, destination) {
   });
 }
 
-function selectButton(button) {
-  var buttons = document.querySelectorAll(".Button");
-  buttons.forEach(function (btn) {
-    btn.classList.remove("selected");
-  });
+// function selectButton(button) {
+//   var buttons = document.querySelectorAll(".Button");
+//   buttons.forEach(function (btn) {
+//     btn.classList.remove("selected");
+//   });
 
-  button.classList.add("selected");
+//   button.classList.add("selected");
+// }
+
+function selectButton(button) {
+  if (!button.classList.contains('selected')) {
+      button.classList.add('selected');
+  }
+
+ var buttonText = button.querySelector('p').textContent;
+
+  var selectedText = document.getElementById('selectedText');
+  var newSelectedText = document.createElement('p');
+  newSelectedText.textContent = buttonText;
+
+  selectedText.appendChild(newSelectedText);
 }
+
+
+
+
+
 
 
