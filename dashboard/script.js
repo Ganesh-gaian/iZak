3b5500dfc9ebc64e0ba18f6cfd1c2070f0d76532
SaddamGaian <mohammad.saddam@gaiansolutions.com>
2024-06-05 18:19:55 +0530

    added Nav-Animation in Dashboard

--- dashboard/script.js
+++ dashboard/script.js
@@ -1,7 +1,29 @@
-var updatingExists = document.getElementById("status").innerText.includes("Updating");
+var updatingExists = document
+  .getElementById("status")
+  .innerText.includes("Updating");
 
 if (updatingExists) {
   document.getElementById("status").innerText = "Updating | 5 devices";
 } else {
   document.getElementById("status").innerText = "Favourite Devices | 5 devices";
 }
+
+//for side nav animation
+// Get all iconStyle elements
+const icons = document.querySelectorAll('.iconStyle');
+
+// Function to handle icon click event
+function handleIconClick(event) {
+    // Remove 'selected' class from all icons
+    icons.forEach(icon => icon.classList.remove('selected'));
+    // Add 'selected' class to the clicked icon
+    event.currentTarget.classList.add('selected');
+}
+
+// Attach click event listener to each iconStyle element
+icons.forEach(icon => icon.addEventListener('click', handleIconClick));
+
+// Set the Dashboard element as selected by default
+document.querySelector('[data-name="Dashboard"]').classList.add('selected');
+
+
