1b3e473f8bbf307bf65501812ef601b333f3682a
SaddamGaian <mohammad.saddam@gaiansolutions.com>
2024-06-10 17:52:21 +0530

    fixed modals in voiceAssistance & helpAndSupport

--- dashboard/script.js
+++ dashboard/script.js
@@ -8,22 +8,16 @@ if (updatingExists) {
   document.getElementById("status").innerText = "Favourite Devices | 5 devices";
 }
 
-//for side nav animation
-// Get all iconStyle elements
+//for sideNavbar 
 const icons = document.querySelectorAll('.iconStyle');
 
-// Function to handle icon click event
 function handleIconClick(event) {
-    // Remove 'selected' class from all icons
     icons.forEach(icon => icon.classList.remove('selected'));
-    // Add 'selected' class to the clicked icon
     event.currentTarget.classList.add('selected');
 }
 
-// Attach click event listener to each iconStyle element
 icons.forEach(icon => icon.addEventListener('click', handleIconClick));
 
-// Set the Dashboard element as selected by default
 document.querySelector('[data-name="Dashboard"]').classList.add('selected');
 
 
