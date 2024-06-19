de811691aa596456a0330768e05e71a685f03bf0
Mounikagaiansolution <mounika.koseti@gaiansolution.com>
2024-06-19 17:38:06 +0530

    Adding modal in edit playbook screen

--- playbook/editplaybook.js
+++ playbook/editplaybook.js
@@ -1,16 +1,35 @@
-document.querySelector('.icon1').addEventListener('click', function() {
+document.querySelector(".icon1").addEventListener("click", function () {
+  window.location.href = "editplaybooktriggers.html";
+});
+document.querySelector(".icon2").addEventListener("click", function () {
+  window.location.href = "editplaybooktriggers.html";
+});
+document.querySelector(".icon3").addEventListener("click", function () {
+  window.location.href = "editplaybooktriggers.html";
+});
+document.querySelector(".back-arrow").addEventListener("click", function () {
+  window.location.href = "landingpage.html";
+});
 
-    window.location.href = 'editplaybooktriggers.html';
-});
-document.querySelector('.icon2').addEventListener('click', function() {
-    
-    window.location.href = 'editplaybooktriggers.html';
-});
-document.querySelector('.icon3').addEventListener('click', function() {
-    
-    window.location.href = 'editplaybooktriggers.html';
-});
-document.querySelector('.back-arrow').addEventListener('click', function() {
-    
-    window.location.href = 'landingpage.html';
+//for next button popup
+document.addEventListener("DOMContentLoaded", function () {
+  const openButton = document.getElementById("openPopUp");
+  const popup = document.getElementById("popupp");
+
+  if (openButton && popup) {
+    openButton.addEventListener("click", function () {
+      popup.style.display = "block";
+    });
+
+    document.addEventListener("click", function (event) {
+      if (
+        !event.target.closest("#popupp .popup-content") &&
+        event.target !== openButton
+      ) {
+        popup.style.display = "none";
+      }
+    });
+  } else {
+    console.error("One or more elements are missing: openButton, popup");
+  }
 });
